2e11037e714e66300f03cecdeb512bf7db9244e6
Yasser Ahmed <yassermarvel8@gmail.com>
2026-09-22 15:42:40 +0530

    cleaned the workspace

--- tabulate.py
+++ tabulate.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-input_path = "D:\CFTR\clinvar_result_final.txt"
-output_path = "D:\CFTR\clinvar_result_final_tabulated.xlsx"
+input_path = "datasets/clinvar_result_final.txt"
+output_path = "datasets/clinvar_result_final_tabulated.xlsx"
 
 # Read as tab-separated, keep everything as string to avoid any implicit conversions
 df = pd.read_csv(input_path, sep="\t", dtype=str, keep_default_na=False)
